80aee4b65536260e1ed9b83ec2599f7cb4e5cff1
AliNL <584342218@qq.com>
2015-12-21 17:20:22 +0800

    C2
    add select

--- AngularJS_Underscore-master/javascripts/app.js
+++ AngularJS_Underscore-master/javascripts/app.js
@@ -4,5 +4,11 @@
   var myAPP = angular.module('myAPP', []);
   myAPP.controller('mainController', function($scope) {
     $scope.books = data.books;
+    $scope.publishers = _.uniq(_.map(data.books,function(book){
+      return book.publisher;
+    }));
+    $scope.booktags = _.uniq(_.flatten(_.map(data.books,function(book){
+      return book.tags;
+    })));
   });
 })(angular, data, _);
