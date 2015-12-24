2808b3997a5c028c11680aef8bdfc2f228b61c99
AliNL <584342218@qq.com>
2015-12-24 15:56:51 +0800

    C3
    ok

--- AngularJS_Underscore-master/javascripts/app.js
+++ AngularJS_Underscore-master/javascripts/app.js
@@ -4,11 +4,19 @@
   var myAPP = angular.module('myAPP', []);
   myAPP.controller('mainController', function($scope) {
     $scope.books = data.books;
-    $scope.publishers = _.uniq(_.map(data.books,function(book){
-      return book.publisher;
-    }));
-    $scope.booktags = _.uniq(_.flatten(_.map(data.books,function(book){
-      return book.tags;
-    })));
+    $scope.booksFiltered = data.books;
+    $scope.publishers = _.uniq(_.map(data.books,function(book){return book.publisher;}));
+    $scope.booktags = _.uniq(_.flatten(_.map(data.books,function(book){return book.tags;})));
+    $scope.change = function(){
+			var temp = $scope.books;
+    	if(!_.isNull($scope.publisher))
+    		temp = _.where($scope.books, {publisher: $scope.publisher});
+    	if(!_.isNull($scope.booktag)){
+    		temp = _.filter(temp, function(book){
+    			return _.contains(_.pluck(book.tags, 'name'), $scope.booktag);
+    		});
+    	}
+    	$scope.booksFiltered = temp;
+    }
   });
 })(angular, data, _);
