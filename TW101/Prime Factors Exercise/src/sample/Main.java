88c034c867d94a07b34c06a837eddb3504c98d29
AliNL <584342218@qq.com>
2016-02-16 15:20:44 +0800

    commit 9
    finish the Prime Factors Exercise

---
+++ TW101/Prime Factors Exercise/src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static List<Integer> generate(int n){
+        List<Integer> list = new ArrayList();
+        for(int i=2;i<=n;i++){
+            if(n%i==0){
+                list.add(i);
+                while(n%i==0)
+                    n/=i;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print(generate(n));
+    }
+}
